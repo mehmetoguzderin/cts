fdc1f2d238a00eb79f11d04f7446b00b836d6348
mehmetoguzderin <mehmetoguzderin@mehmetoguzderin.com>
2020-12-07 18:13:04 +0000

    Deploying to gh-pages from  @ 8ed4f48efc545d0a4dcd2dee4b7bf53ebb3d3120 🚀

--- out-wpt/common/framework/version.js
+++ out-wpt/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '15901d497095b475ae410d7d59fdf771880d789e';
+export const version = '8ed4f48efc545d0a4dcd2dee4b7bf53ebb3d3120';
